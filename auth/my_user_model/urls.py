28154f29dd87d634d7b79d9e8091de20fa0e18be
LeudiX <leudix.rafael@gmail.com>
2024-09-06 19:18:23 -0400

    Fixed responsive behaviour of navbar and forms components

--- auth/my_user_model/urls.py
+++ auth/my_user_model/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('register/',views.register, name='register'),
     path('base/',views.base,name='base'),
     path('home/',views.home, name='home'),
+    path('',views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
 ]
