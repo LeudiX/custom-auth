46029fd4e89a8ea1a439bd0d9a6c4e362e14bc20
LeudiX <leudix.rafael@gmail.com>
2024-09-08 12:00:33 -0400

    Conducted migrations over new Inquiries model

--- auth/inquiries/models.py
+++ auth/inquiries/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-CustomUser = get_user_model
+CustomUser = get_user_model()
 
 class AdministrativeRequests(models.Model):
 
