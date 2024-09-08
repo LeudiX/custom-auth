d7644c6a7dc0a772e8c9c4290e696dd58c2ea478
LeudiX <leudix.rafael@gmail.com>
2024-09-08 18:11:58 -0400

    Added CRUD logic to Academic Requests view

--- auth/inquiries/views.py
+++ auth/inquiries/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.forms import BaseModelForm
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
@@ -19,7 +20,8 @@ class AdministrativeRequestsCreateView(CreateView):
         # Attach the current request to the form instance before saving
         form.instance.sender = self.request.user
         return super().form_valid(form)
-
+    
+    # reverse returns the string url path
     def get_success_url(self) -> str:
         return reverse_lazy('administrativerequests-list')
     
@@ -49,6 +51,7 @@ class AdministrativeRequestsUpdateView(UpdateView):
     fields = ['subject','body','receiver','reply']
     template_name = 'inquiries/admin_requests_details.html'
 
+    # reverse returns the string url path   
     def get_success_url(self) -> str:
         return reverse_lazy('administrativerequests-list')
 
@@ -89,7 +92,7 @@ class AdmininstrativeRequestsListView(ListView):
         elif user.is_student():
             return AdministrativeRequests.objects.filter(sender = user)
     
-        # If the user is team lead, return all administrative requests
+        # If the user is teamlead, return all administrative requests
         elif user.is_teamlead():
             return AdministrativeRequests.objects.all()
 
@@ -98,6 +101,7 @@ class AdministrativeRequestsDeleteView(DeleteView):
     model = AdministrativeRequests
     template_name = 'inquiries/confirm-delete.html'
 
+    # reverse returns the string url path
     def get_success_url(self):
         return reverse_lazy('administrativerequests-list')
     
@@ -107,13 +111,136 @@ class AdministrativeRequestsDeleteView(DeleteView):
         
         # Retrieves the subject of the administrativerequest we want to delete
         administration_request_subject = AdministrativeRequests.objects.get(pk = self.kwargs.get('pk')).subject
-        context['delete-confirm-message'] = f'Are you sure yo want to delete this Administrative Request:"{administration_request_subject}"?'
+        context['confirm-delete-message'] = f'Are you sure yo want to delete this Administrative Request:"{administration_request_subject}"?'
 
         # Display a cancel button on confirmation page and if pressed , return the user to the academicrequests list 
         context['cancel-url'] = 'administrativerequests-list'
         return context
         
 #-------------------------------------------------------------------------------------------------------------------------------        
-
+ 
+class AcademicRequestsCreateView(CreateView):
     
+    model = AcademicRequests
+    fields = ['subject', 'body', 'receiver', 'reply']
+    template_name = 'inquiries/academic_requests_details.html'
+
+
+    def form_valid(self, form):
+        # Attach the current request to the form instance before saving
+        form.instance.sender = self.request.user
+        return super().form_valid(form)
+    
+    # reverse returns the string url path
+    def get_success_url(self):
+        return reverse_lazy('academicrequests-list')
+
+    # Adds a title to the context by calling the Parent's class get_context_data and extending it
+    # Parent class in this case is CreateView
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['table_title'] = 'Add new Academic Request' 
+        return context
+     
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+
+        # Check is the user is student
+        if self.request.user.is_student():
+            # Make the 'reply' field read-only so that the field can still be submitted
+            form.fields['reply'].widget.attrs['readonly']= True
+        
+        return form
+
+class AcademicRequestsUpdateView(UpdateView):
+    
+    model = AcademicRequests
+    fields = ['subject','body','receiver','reply']
+    template_name = 'inquiries/academic_requests_details.html'
+
+    # reverse returns the string url path
+    def get_success_url(self):
+        return reverse_lazy('academicrequests-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["table_title"] = 'Update Academic Request'
+        return context
+
+    def get_form(self, form_class = None):
+        form = super().get_form(form_class)
+
+        # Check if the user is student or teamlead
+        if self.request.user.is_student() or self.request.user.is_teamlead():
+            # Make the 'reply' field read-only for students and teamleads, so that the
+            # field can still be submitted
+            form.fields['reply'].widget.attrs['readonly'] = True
+            
+        return form
+                
+class AcademicRequestsListView(LoginRequiredMixin,ListView):
+    
+    model = AcademicRequests
+
+    # The context object name will be used in the templates to query the academic requests
+    context_object_name = 'academic_requests'
+    template_name = 'inquiries/academic_requests_list.html'
+    paginate_by = 5
+
+    def get_queryset(self):
+        
+        # Get the currently logged-in user
+        user = self.request.user
+        
+        #If the user is facilitator, filter academic requests where the receiver is the facilitator
+        if user.is_facilitator():
+            return AcademicRequests.objects.filter(receiver = user)
+
+        # If the user is a student, filter academic requests sent by the student
+        elif user.is_student():
+            return AcademicRequests.objects.filter(sender = user)  
+
+        # If the user is a teamlead, return all academic requests 
+        elif user.is_teamlead():
+            return AcademicRequests.objects.all()
+        
+class AcademicRequestsDeleteView(DeleteView):
+    
+    model = AcademicRequests
+    template_name = 'inquiries/confirm-delete.html'
+
+    # reverse returns the string url path
+    def get_success_url(self):
+        return reverse_lazy('academicrequests-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Delete Academic Request' 
+        
+        # Retrieves the subject of the academic request we want to delete
+        academic_request_subject = AcademicRequests.objects.get(pk =self.kwargs.get('pk')).subject
+        context['confirm-delete-message'] = f'Are you sure you want to delete this academic request: "{academic_request_subject}" ?'
+        # Display a cancel button on confirmation page and if pressed, return user to the academic requests list
+        context['cancel_url'] = 'academicrequests-list'
+        return context
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
